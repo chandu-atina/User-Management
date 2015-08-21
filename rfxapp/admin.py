f5834985e99a9723d26694013f2b6577235571e8
chandu-atina <chandu.atina@gmail.com>
2015-08-21 14:13:24 +0530

    First commit

---
+++ rfxapp/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Question
+from .models import Choice
+
+# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+#admin.site.register(Question, QuestionAdmin)
+
+class QuestionAdmin1(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        #('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    
+    
+admin.site.register(Question, QuestionAdmin1) 
+#admin.site.register(Choice)
